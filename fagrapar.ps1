775c268dc8a3118c46029102c6ff1587fd38a288
Roman Bolshakov <rombolshak@yandex.ru>
2017-04-05 11:33:05 +0300

    get links from file

--- fagrapar.ps1
+++ fagrapar.ps1
@@ -1,1 +1,19 @@
-﻿
+﻿Param(
+	[Parameter(Mandatory=$true)][string]$InputFile,
+	[string]$OutputFile = "results.csv",
+	[string]$Proxy = $null)
+
+Set-StrictMode -Version "3.0"	
+$ErrorActionPreference = "Stop"
+
+
+if (-not (Test-Path $InputFile))
+{
+    throw "No such file: $InputFile"
+}
+
+$links = Get-Content $InputFile
+
+$resultFile = New-Item -Path $PSScriptRoot -Name $OutputFile -ItemType File -Force 
+
+Write-Host "Total $($links.Length) links"
