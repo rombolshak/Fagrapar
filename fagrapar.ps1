15fecdb372e4b7016aa5c5db17e69a7d9e5011b7
Roman Bolshakov <rombolshak@yandex.ru>
2017-04-06 00:39:46 +0300

    parallel saving, flattening object

--- fagrapar.ps1
+++ fagrapar.ps1
@@ -1,11 +1,32 @@
 ﻿Param(
 	[Parameter(Mandatory=$true)][string]$InputFile,
+	[Parameter(Mandatory=$true)][string]$Proxy = $null,
 	[string]$OutputFile = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot "results.csv")),
-	[string]$Proxy = $null)
+    [switch]$JustCollectResults)
 
 Set-StrictMode -Version "3.0"	
 $ErrorActionPreference = "Stop"
 Import-Module $PSScriptRoot\SplitPipeline
+
+$resultsDirectory = New-Item `
+    -Name ([System.IO.Path]::GetFileNameWithoutExtension($OutputFile)) `
+    -Path ([System.IO.Path]::GetDirectoryName($OutputFile)) `
+    -ItemType Directory -Force
+
+function CollectResults()
+{
+    Write-Host "Creaing result file"
+    Get-ChildItem $resultsDirectory |% { Import-Csv $_.FullName } | Export-Csv -Encoding UTF8 -NoTypeInformation -Path $OutputFile
+    Write-Host "Clear temp results directory"
+    Remove-Item $resultsDirectory -Recurse -Force
+    Write-Host "Done"
+}
+
+if ($JustCollectResults)
+{
+    CollectResults
+    exit
+}
 
 if (-not (Test-Path $InputFile))
 {
@@ -32,41 +53,55 @@ if (Test-Path $OutputFile)
     }
 }
 
-Write-Host Output file is $OutputFile
-$links = Get-Content $InputFile
-$data = @{
-    proxy=$Proxy; 
-    done = 0;
-    total = $links.Count;
-    currentDir = $PSScriptRoot;
+$stopwatch = [system.diagnostics.stopwatch]::StartNew()
+
+try
+{
+    Write-Host Output file is $OutputFile
+    $links = Get-Content $InputFile
+    $data = @{
+        proxy=$Proxy; 
+        done = 0;
+        failed = 0;
+        total = $links.Count;
+        currentDir = $PSScriptRoot;
+    }
+
+    $links | Split-Pipeline -Variable data, resultsDirectory -Script `
+    {process{
+
+        Import-Module "$($data.currentDir)\LockObject"
+        . "$($data.currentDir)\ConvertTo-FlatObject.ps1"
+        $uri = $_
+
+        try 
+        {
+            $id = [System.Guid]::NewGuid();
+            Invoke-WebRequest -Uri $uri `
+                -Proxy $data.proxy -ProxyUseDefaultCredentials `
+                | ConvertFrom-Json `
+                | ConvertTo-FlatObject `
+                | Export-Csv -Encoding UTF8 -NoTypeInformation -Path "$resultsDirectory\$id.csv"
+        }
+        catch
+        {
+            Write-Host "$($_.Exception.Message) URI: $($uri.Substring(0, 50))..."
+            Lock-Object $data { ++$data.failed }
+        }
+        finally
+        {
+            Lock-Object $data { $done = ++$data.done }
+            Write-Progress -Activity "Done $done of $($data.total)" -Status Processing -PercentComplete (100*$done/$data.total)
+        }
+    }}
+
+}
+finally
+{
+    $stopwatch.Stop()
+    Write-Host Processed $($data.done - $data.failed) links of $($data.total)
+    Write-Host Failed $data.failed "request(s)"
+    Write-Host Total time: $($stopwatch.Elapsed)
 }
 
-$links | Split-Pipeline -Variable data, OutputFile -Verbose -Script `
-{process{
-
-    Import-Module "$($data.currentDir)\LockObject"
-    function ExecuteInMonitor($lock, [scriptblock]$script)
-    {
-        [System.Threading.Monitor]::Enter($lock)
-        try { &$script }
-        finally {[System.Threading.Monitor]::Exit($lock)}
-    }
-
-    $uri = $_
-    try 
-    {
-        Invoke-WebRequest -Uri $uri -Proxy $data.proxy -ProxyUseDefaultCredentials `
-        | ConvertFrom-Json `
-        | Export-Csv -Encoding UTF8 -NoTypeInformation -Path $OutputFile -Append
-        Start-Sleep 1000
-    }
-    catch
-    {
-        Write-Host "$($_.Exception.Message) URI: $($uri.Substring(0, 50))..."
-    }
-    finally
-    {
-        Lock-Object $data { $done = ++$data.Done } -Verbose
-        Write-Progress -Activity "Done $done" -Status Processing -PercentComplete (100*$done/$data.total)
-    }
-}}
+CollectResults
