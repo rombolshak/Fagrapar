d03faa7def2860c4b9261c033e64b74646bba956
Roman Bolshakov <rombolshak@yandex.ru>
2017-04-26 00:05:38 +0300

    use proxy

---
+++ Commands/Commands/ParseWherevent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+using AngleSharp.Extensions;
+using AngleSharp.Parser.Html;
+
+namespace Commands
+{
+	[Cmdlet("Parse", "Wherevent")]
+	public class ParseWherevent : Cmdlet
+	{
+		[Parameter(Mandatory = true, Position = 0)]
+		public string Url { get; set; }
+
+		[Parameter(Mandatory = true)]
+		public string Proxy { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			var client = new WebClient
+			{
+				Encoding = Encoding.UTF8,
+				Proxy = new WebProxy { Address = new Uri(Proxy), UseDefaultCredentials = true }
+			};
+			var response = client.DownloadString(Url);
+			var parser = new HtmlParser();
+			var document = parser.Parse(response);
+			var events = document.QuerySelectorAll(".event");
+			foreach (var @event in events)
+			{
+				WriteObject(new
+				{
+					Url = Url,
+					Thumb = @event.QuerySelector("img").GetAttribute("src"),
+					Title = @event.QuerySelector(".event_title").Text(),
+					DateTime = @event.QuerySelector("time").GetAttribute("datetime"),
+					Location = @event.QuerySelector(".event_location").Text().Trim(),
+					FemaleCount = @event.QuerySelector(".event_femalecount").Text(),
+					MaleCount = @event.QuerySelector(".event_malecount").Text()
+				});
+			}
+		}
+	}
+}
